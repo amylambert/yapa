2a7a12c4758df114ec5da4e04c89129a06f141f0
amylambert <amy.lambert@gmail.com>
2026-06-26 11:00:19 +0200

    remove non useful migration files at this state

--- projects/views/__init__.py
+++ projects/views/__init__.py
@@ -2,5 +2,4 @@ from .register import RegisterView
 from .project_create import ProjectCreateView
 from .project_delete import ProjectDeleteView
 from .project_detail import ProjectDetailView
-from .project_list import ProjectListView
 from .project_inline_update import ProjectInlineUpdateView
