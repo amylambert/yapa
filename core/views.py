ed3c8ba433c47a9b6cc6834c10748b9a66d518d8
amylambert <amy.lambert@gmail.com>
2026-06-28 21:26:44 +0200

    Add google calendar iframe view + google calendar url on .env

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import os
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
@@ -6,3 +7,14 @@ class DashboardView(LoginRequiredMixin, generic.TemplateView):
     """Renders the primary authenticated landing framework."""
 
     template_name = "dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        """Injects required environmental variables into template scope."""
+        context = super().get_context_data(**kwargs)
+        
+        # Read the environment string cleanly using the OS system wrapper
+        context["calendar_embed_url"] = os.environ.get(
+            "GOOGLE_CALENDAR_EMBED_URL", ""
+        )
+        
+        return context
